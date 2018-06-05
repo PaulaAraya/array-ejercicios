464a2055e5f399d52b0f2957d051d248393666f6
PaulaAraya <paula.arayawilliams@gmail.com>
2018-06-05 12:45:18 -0300

    ejercicio 1 retos de codigo

---
+++ Retos de Código/app.js
@@ -0,0 +1,9 @@
+/* 1) computeAverageLengthOfWords
+Escriba una función 'computeAverageLengthOfWords' que recibe dos strings y devuelve el promedio de la longitud de ambos strings.
+*/
+
+function computeAverageLengthOfWords(word1, word2) {
+  return (word1.length + word2.length)/2
+}
+
+module.exports = computeAverageLengthOfWords;
