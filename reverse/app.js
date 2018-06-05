3680d448b7b8447009e1280aba7e902ea9ca00d9
PaulaAraya <paula.arayawilliams@gmail.com>
2018-06-04 23:09:02 -0300

    first commit

---
+++ reverse/app.js
@@ -0,0 +1,11 @@
+// metodo reverse en array
+
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "y", "j", "k"];
+const invested = alphabet.reverse ;
+console.log(invested) 
+
+// array invertido es un nuevo array
+const alphabet2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+    alphabet2.sort( function (a,b) {
+      return b.localeCompare(a);
+  });
